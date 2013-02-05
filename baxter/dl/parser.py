72277c22bbabc544be72712e052ec2cf25225bcd
Bartek Borowinski <bborowin@gmail.com>
2013-02-05 01:54:58 -0500

    .

---
+++ baxter/dl/parser.py
@@ -0,0 +1,11 @@
+"""
+  Logic for parsing raw story data into structured Stories
+"""
+
+from baxter.dl.models import Source, Story, StoryElement, ExtractCmd, SoupCmd, ParserCmd
+
+class Parser:
+  
+  # load a story 
+  def load(url):
+    pass
